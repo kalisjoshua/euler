250c57620d481cdf4845d2eff16ec50e210c1697
Joshua T Kalis <kalisjoshua@gmail.com>
2013-01-13 13:50:45 -0500

    learning haskell

--- haskell/euler1.hs
+++ haskell/euler1.hs
@@ -4,4 +4,6 @@
 
   Find the sum of all the multiples of 3 or 5 below 1000.
   -}
-sum [x | x <- [1..999], mod x 5 == 0 || mod x 3 == 0]
+
+main = 
+  print (sum [x | x <- [1..999], mod x 5 == 0 || mod x 3 == 0])
