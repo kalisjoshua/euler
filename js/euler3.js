1c5d8f45fc03773450624213d0723cfeb0c77d3b
Joshua T Kalis <kalisjoshua@gmail.com>
2013-07-12 09:24:06 -0400

    complete euler #3 in JS

---
+++ js/euler3.js
@@ -0,0 +1,16 @@
+/*
+  The prime factors of 13195 are 5, 7, 13 and 29.
+
+  What is the largest prime factor of the number 600851475143 ?
+ */
+
+function factors (num, i) {
+  i = 2;
+
+  while (num % i !== 0 && i++);
+
+  return [i]
+    .concat((num / i > 1) ? factors(num / i) : []);
+}
+
+console.log(factors(600851475143));
