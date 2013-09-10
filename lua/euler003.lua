55f3d258172ea89e951fa67e47ba59334bb633c5
Joshua T Kalis <kalisjoshua@gmail.com>
2013-09-10 16:46:20 -0400

    update, add helper and reporting to all lua solutions

--- lua/euler003.lua
+++ lua/euler003.lua
@@ -1,10 +1,10 @@
+--[[
+  The prime factors of 13195 are 5, 7, 13 and 29.
 
--- The prime factors of 13195 are 5, 7, 13 and 29.
+  What is the largest prime factor of the number 600851475143 ?
+  ]]
 
--- What is the largest prime factor of the number 600851475143 ?
-
-local ANSWER = 6857
-local number = 600851475143
+local helper = require("lua/helper").helper
 
 function factors (num)
   local i = 2
@@ -20,10 +20,4 @@ function factors (num)
   end
 end
 
-local result = factors(number)
-
-if ANSWER == result then
-  print(true)
-else
-  print(result)
-end
+helper(6857, factors(600851475143))
