d90df58861b73e2d4e85d2e6581f35cf99cf8ce0
Joshua T Kalis <kalisjoshua@gmail.com>
2013-09-10 19:34:45 -0400

    super fast solution

---
+++ lua/euler006.lua
@@ -0,0 +1,28 @@
+--[[
+  The sum of the squares of the first ten natural numbers is,
+
+  12 + 22 + ... + 102 = 385
+
+  The square of the sum of the first ten natural numbers is,
+
+  (1 + 2 + ... + 10)2 = 552 = 3025
+
+  Hence the difference between the sum of the squares of the first
+  ten natural numbers and the square of the sum is 3025  385 = 2640.
+
+  Find the difference between the sum of the squares of the
+  one hundred natural numbers and the square of the sum.
+  ]]
+
+local helper = require('lua/helper').helper
+local limit = 100
+local result = {0, 0}
+
+while limit > 0 do
+  result = {result[1] + limit * limit, result[2] + limit}
+  limit = limit - 1
+end
+
+result = result[2] ^ 2 - result[1]
+
+helper(25164150, result);
