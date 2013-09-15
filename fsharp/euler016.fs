24f6b7f3d03b47b8089e754c8526b06e2186d98d
John Fair <johnbfair@gmail.com>
2013-09-15 14:57:38 -0400

    Updated formatting to make the sol'n easier to read.  Also changed Int64 to Int16 (Int64 isn't necessary here as we're dealing w/ individual ints < 10)

--- fsharp/euler016.fs
+++ fsharp/euler016.fs
@@ -1,3 +1,7 @@
 module euler016 = 
+    open System
     // bitshifting broke this, but somehow bigint was ok with it...
-    let result = (bigint(2)**1000).ToString() |> Seq.map (fun x-> Int64.Parse(x.ToString()))|> Seq.reduce(+)
+    let result = 
+        (bigint(2)**1000).ToString() 
+        |> Seq.map (fun x-> x |> string |> Int16.Parse)
+        |> Seq.reduce(+)
